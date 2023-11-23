e19708a7fadfb0d28b7ce016749894343c8de8ff
Nano11Bravo <samuel.walton@seven-labs.com>
2023-11-23 09:15:33 -0500

    YA404

--- error-pages/script.js
+++ error-pages/script.js
@@ -1,99 +1,145 @@
-var 
-	yetiTL, chatterTL,
-    	furLightColor = "#FFF",
-    	furDarkColor = "#67b1e0",
-    	skinLightColor = "#ddf1fa",
-    	skinDarkColor = "#88c9f2",
-    	lettersSideLight = "#3A7199",
-    	lettersSideDark = "#051d2c",
-    	lettersFrontLight = "#67B1E0",
-    	lettersFrontDark = "#051d2c",
-    	lettersStrokeLight = "#265D85",
-    	lettersStrokeDark = "#031219",
-    	mouthShape1 = "M149 115.7c-4.6 3.7-6.6 9.8-5 15.6.1.5.3 1.1.5 1.6.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.4-1-.7-1.5-2.8-5.2-8.4-8.3-14.1-7.9-3.7.2-5.9 1.1-8.7 3.2z",
-    	mouthShape2 = "M161.2 118.9c0 2.2-1.8 4-4 4s-4-1.8-4-4c0-1 .4-2 1.1-2.7.7-.8 1.8-1.3 2.9-1.3 2.2 0 4 1.7 4 4z",
-    	mouthShape3 = "M150.2 118.3c-4.6 3.7-7.5 6.4-6.3 12.3.1.5.1.6.3 1.1.6 1.5 2.4 2.3 3.9 1.7 0 0 7.9-4.3 10.7-5.5s11.6-3.3 11.6-3.3c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.2-.6-.4-1.1-2.8-5.2-7.1-4.9-12.9-4.6-3.7.4-6.3 1.5-8.6 3.3z",
-    	mouthShape4 = "M149.2 116.7c-4.6 3.7-6.7 8.8-5.2 14.6.1.3.1.5.2.8.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.1-.3-.2-.5-.4-.7-2.8-5.2-8.2-7.2-14-6.9-3.6.2-5.9 1.1-8.6 3.2z"
-;
+const BLACKLISTED_KEY_CODES = [38,40,37,39,18,20,17,16,9,27,144];
+//List of commands
+const COMMANDS = {
+    "help":
+        'The page you want to visit does not exist, or it may have been deleted, or the wrong address was entered. To see the commands, enter the word <span class=\"red\"> commands</span>',
+    "exit":
+        "",
+    "report":
+        "<span class='green'>This page report has been successfully sent to support.</span>",
+    "commands":
+        "List of commands: <span class=\"red\"> help</span> , <span class=\"red\"> report</span> ,<span class=\"red\"> exit</span>\n",
+    "cls":
+        ""
+};
 
-chatterTL = new TimelineMax({paused: true, repeat: -1, yoyo: true});
-chatterTL
-	.to(['#mouthBG', '#mouthPath', '#mouthOutline'], .1, {morphSVG: mouthShape4}, "0")
-	//.to('#armR', .1, {x: 2, ease: Linear.easeNone}, "0")
-	.to('#chin', .1, {y: 1.5}, "0")
-;
+let userInput
+let terminalOutput
+let Terminal
+let Keyboard
+const app = () => {
+    userInput = document.getElementById("userInput");
+    terminalOutput = document.getElementById("code");
+    Terminal = document.getElementById("Terminal");
+    Keyboard = document.getElementById("Keyboard");
+    Keyboard.focus();
+    if (screen.width < 991){
+        Keyboard.addEventListener("keyup", key);
+    }
+    else{
+        document.addEventListener('keypress', key);
+    }
+    document.addEventListener("keydown", backSpace);
+};
 
-yetiTL = new TimelineMax({paused: true, repeat: -1, repeatDelay: 0, delay: 0});
-yetiTL
-	.addCallback(function() {
-		chatterTL.play();	
-	}, "0")
-	
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "2.5")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "2.575")
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "2.65")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "2.725")
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "2.8")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "2.875")
+//When the user click the 'Enter' key
+const execute = function executeCommand(input) {
+    let output;
 
-	.addCallback(goLight, "3.2")
-	.addCallback(goDark, "3.3")
-	.addCallback(goLight, "3.4")
+    if (input.length === 0) {
+        return;
+    }
+    //If what the user entered is not in the command list
+    if (!COMMANDS.hasOwnProperty(input)) {
+        output = `<p>The command entered is not correct</p>`;
+    }
+    //If user enter the word cls
+    else if (input === "cls") {
+        terminalOutput.innerHTML = "";
+        return;
+    }
+    //If the user enters one of the words 'exit' and 'close'
+    else if (input === "close" || input === "exit") {
+        document.location.href = "../../index.html" // The link that the user enters after sending the exit
+        return;
+    }
+    //If the user enters the word report
+    else if (input === "report") {
+        terminalOutput.innerHTML = `${
+            terminalOutput.innerHTML
+        }<p>${COMMANDS[input]}</p>`;
 
-	.addCallback(function() {
-		chatterTL.pause();
-		TweenMax.to(['#mouthBG', '#mouthPath', '#mouthOutline'], .1, {morphSVG: mouthShape1}, "0");
-	}, "3.2")
+        return;
+    }
+    //Otherwise, the text of the command will be displayed as output
+    else {
+        output = COMMANDS[input];
+    }
 
-	.to(['#mouthBG', '#mouthPath', '#mouthOutline'], .25, {morphSVG: mouthShape2}, "5")
-	.to('#tooth1', .1, {y: -5}, "5")
-	.to('#armR', .5, {x: 10, y: 30, rotation: 10, transformOrigin: "bottom center", ease: Quad.easeOut}, "4")
-	.to(['#eyeL', '#eyeR'], .25, {scaleX: 1.4, scaleY: 1.4, transformOrigin: "center center"}, "5")
-
-	.addCallback(goDark, "8")
-	.addCallback(goLight, "8.1")
-	.addCallback(goDark, "8.3")
-	.addCallback(goLight, "8.4")
-	.addCallback(goDark, "8.6")
-
-	.to(['#mouthBG', '#mouthPath', '#mouthOutline'], .25, {morphSVG: mouthShape1}, "9")
-	.to('#tooth1', .1, {y: 0}, "9")
-	.to('#armR', .5, {x: 0, y: 0, rotation: 0, transformOrigin: "bottom center", ease: Quad.easeOut}, "9")
-	.to(['#eyeL', '#eyeR'], .25, {scaleX: 1, scaleY: 1, transformOrigin: "center center"}, "9")
-	.addCallback(function() {
-		chatterTL.play();
-	}, "9.25")
-
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "11.5")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "11.575")
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "11.65")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "11.725")
-	.to(['#armL', '#flashlightFront'], .075, {x: 7}, "11.8")
-	.to(['#armL', '#flashlightFront'], .075, {x: 0}, "11.875")
-
-;
-
-function goDark() {
-	TweenMax.set('#light', {visibility: "hidden"});
-	
-	TweenMax.set('.lettersSide', {fill: lettersSideDark, stroke: lettersStrokeDark});
-	TweenMax.set('.lettersFront', {fill: lettersFrontDark, stroke: lettersStrokeDark});
-	TweenMax.set('#lettersShadow', {opacity: .05});
-	
-	TweenMax.set('.hlFur', {fill: furDarkColor});
-	TweenMax.set('.hlSkin', {fill: skinDarkColor});
+    terminalOutput.innerHTML = `${
+        terminalOutput.innerHTML
+    }<p class="out_code">${output}</p>`;
+    Terminal.scrollTop = terminalOutput.scrollHeight;
+};
+let str = '';
+//when user click any key
+const key = function keyEvent(event) {
+    let currentKey = event.key;
+    Keyboard.focus();
+    Keyboard.innerHTML = event.target.value;
+    if (BLACKLISTED_KEY_CODES.includes(event.keyCode)) {
+        return
+    }
+    if (!currentKey || currentKey === "Unidentified" || screen.width < 991) {
+        currentKey = event.target.value;
+    }
+    if (event.key === "Enter") {
+        execute(userInput.innerHTML);
+        userInput.innerHTML = "";
+        currentKey = "";
+        event.target.value = "";
+        str = '';
+    }
+    else{
+        if(screen.width < 991){
+            str = currentKey;
+        }else{
+            str += currentKey;
+        }
+        event.preventDefault();
+        userInput.innerHTML = str;
+    }
 }
-
-function goLight() {
-	TweenMax.set('#light', {visibility: "visible"});
-	
-	TweenMax.set('.lettersSide', {fill: lettersSideLight, stroke: lettersStrokeLight});
-	TweenMax.set('.lettersFront', {fill: lettersFrontLight, stroke: lettersStrokeLight});
-	TweenMax.set('#lettersShadow', {opacity: .2});
-	
-	TweenMax.set('.hlFur', {fill: furLightColor});
-	TweenMax.set('.hlSkin', {fill: skinLightColor});
+//when user click 'BackSpace' key
+const backSpace = function backSpace(e){
+    //if user click backspace
+    if (e.keyCode === 8) {
+        userInput.innerHTML = userInput.innerHTML.slice(
+            0,
+            userInput.innerHTML.length - 1
+        );
+        str = str.slice(
+            0,
+            str.length - 1
+        );
+    }
 }
+//When the user clicks on a control buttons
+const BTNS = function MenuBTN(t) {
+    switch (t) {
+        case "max":
+            if (document.getElementById("body").className !== "max") {
+                document.getElementById("body").className = "max";
+            } else {
+                document.getElementById("body").className = "";
+            }
+            break;
+        case "min":
+            if (document.getElementById("body").className === "max") {
+                document.getElementById("body").className = "max min";
+            } else if (document.getElementById("body").className !== "max") {
+                document.getElementById("body").className = "min";
+            }
+            break;
+        case "re":
+            if (document.getElementById("body").className === "max min") {
+                document.getElementById("body").className = "max";
+            }
+            if (document.getElementById("body").className === "min") {
+                document.getElementById("body").className = "";
+            }
+            break;
+    }
+};
 
-goDark();
-yetiTL.play();
+document.addEventListener("DOMContentLoaded", app);
